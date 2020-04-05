838c7cbc60cb9071400aa9639c5405b64b0eab58
Maria Carter <59865245+mcarter-00@users.noreply.github.com>
2020-04-04 21:46:05 -0700

    Adding plots.

--- plots.js
+++ plots.js
@@ -1,47 +1,127 @@
 /*
- * Generate Dropdown Menu Items: ID numbers
+ * Generate Dropdown Menu Items: ID numbers.
  */
 function init() {
   var selector = d3.select('#selDataset');
 
-  d3.json('samples.json').then(data => {
-    console.log(data);
+  d3.json('samples.json').then((data) => {
     var sampleNames = data.names;
-    sampleNames.forEach(sample => {
-      selector
-        .append('option')
-        .text(sample)
-        .property('value', sample);
+    sampleNames.forEach((sample) => {
+      selector.append('option').text(sample).property('value', sample);
     });
   });
 }
 
 init();
 /*
-! NOTE: The function optionChanged() is called when a change takes place in 
-! in the drop-down menu from HTML. [this.value = newSample aka ID number]
+! NOTE: optionChanged is called when a change takes place in this tag in the HTML. 
+! this.value = newSample = ID number
 */
 function optionChanged(newSample) {
   buildMetadata(newSample);
   buildCharts(newSample);
 }
 /*
- * optionChanged calls two other functions: buildMetadata() and buildCharts()
- */
-
-/*
- * Print information to the Demographic Info panel once a user selects an ID number
+ * Print information to the "Demographic Info" once a user selects an ID number.
  */
 function buildMetadata(sample) {
-  d3.json('samples.json').then(data => {
+  d3.json('samples.json').then((data) => {
     var metadata = data.metadata;
-    var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
+    var resultArray = metadata.filter((sampleObj) => sampleObj.id == sample);
     var pairs = Object.entries(resultArray[0]);
     var PANEL = d3.select('#sample-metadata');
 
     PANEL.html('');
-    var results = pairs.forEach(function(pair) {
+    var results = pairs.forEach(function (pair) {
       PANEL.append('h6').text(pair[0] + ': ' + pair[1]);
     });
+  });
+}
+/*
+ * Build the charts once a user selects an ID number.
+ */
+function buildCharts(sample) {
+  d3.json('samples.json').then(function ({ samples, metadata }) {
+    var data = samples.filter((obj) => obj.id == sample)[0];
+    console.log(data);
+    // data for bar chart
+    var otuIDS = data.otu_ids.map((row) => `OTU ID: ${row}`);
+    var sampleValues = data.sample_values.slice(0, 10);
+    var sampleLabels = data.otu_labels.map((label) =>
+      label.replace(/\;/g, ', ')
+    );
+    // data for bubble chart
+    var otuID = data.otu_ids;
+    var sampleValue = data.sample_values;
+    var sampleLabel = data.otu_labels.map((label) =>
+      label.replace(/\;/g, ', ')
+    );
+    // data for gauge
+    var metData = metadata.filter((obj) => obj.id == sample)[0];
+    console.log(metData);
+    var washFreq = metData.wfreq;
+    console.log(washFreq);
+
+    // data for bar chart
+    var data1 = [
+      {
+        x: sampleValues,
+        y: otuIDS,
+        type: 'bar',
+        orientation: 'h',
+        text: sampleLabels,
+        hoverinfo: 'text',
+      },
+    ];
+
+    // data for bubble chart
+    var data2 = [
+      {
+        x: otuID,
+        y: sampleValue,
+        mode: 'markers',
+        text: sampleLabel,
+        marker: {
+          size: sampleValue,
+          color: otuID,
+        },
+      },
+    ];
+    // data for gauge chart
+    var data3 = [
+      {
+        // domain: washFreq,
+        value: washFreq,
+        title: {
+          text: 'Belly Button Washing Frequency (Scrubs per Week)',
+        },
+        type: 'indicator',
+        mode: 'gauge+number',
+        gauge: {
+          axis: { range: [null, 10] },
+        },
+      },
+    ];
+    // layout for bar chart
+    var layout1 = {
+      margin: {
+        t: 40,
+        l: 150,
+      },
+    };
+    // layout for bubble chart
+    var layout2 = {
+      xaxis: { title: 'OTU ID' },
+    };
+    // layout for gauge chart
+    var layout3 = {
+      width: 600,
+      height: 500,
+      margin: { t: 0, b: 0 },
+    };
+
+    Plotly.newPlot('bar', data1, layout1);
+    Plotly.newPlot('bubble', data2, layout2);
+    Plotly.newPlot('gauge', data3, layout3);
   });
 }
